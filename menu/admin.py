b152a027135eb353774621bb2311f894639c6370
ShadeSWD <seawolfy60@gmail.com>
2024-02-07 11:53:09 +0300

    menu items

--- menu/admin.py
+++ menu/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from menu.models import Menu
+from menu.models import Menu, MenuItem
 
 
 @admin.register(Menu)
 class DealerAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'parent')
+    list_display = ('pk', 'name')
 
+
+@admin.register(MenuItem)
+class DealerAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'menu', 'name', 'parent')
+    list_filter = ('menu',)
